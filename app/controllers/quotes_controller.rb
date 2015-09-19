6fe43fa3f4a7276cf2645d968ea4c14e3dcdb651
Kelly Munro <kellymdev@gmail.com>
2015-09-20 11:53:13 +1200

    Updated query for random quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   #get '/quotes/random/:category'
   def random
     category = Category.find_by("title = ?", params["category"])
-    @quote = category.quotes.sample
+    @quote = category.quotes.order("random()").first
     @categories = Category.all
   end
 end
